7d15c6ca95ba70bf1e419ec32416df24c97309c0
CaiJingLong <cjl_spy@163.com>
2023-04-10 09:49:47 +0800

    ✅ Update test

--- test.sh
+++ test.sh
@@ -12,23 +12,23 @@ shift 1
 case $TYPE in
 autotools | at)
     ## Compile autotools example (libffi)
-    C c autotools -C example/libffi-git $@
+    C c autotools -C example/libffi-git-at $@
     ;;
 cmake | cm)
     ## Compile cmake example (zlib)
-    C c cmake -C example/zlib-git $@
+    C c cmake -C example/zlib-git-cmake $@
     ;;
 http)
-    C c cmake -C example/zlib-http $@
+    C c cmake -C example/zlib-http-cmake $@
     ;;
 subpath)
-    C c cmake -C example/libexpat-subpath $@
+    C c cmake -C example/libexpat-subpath-cmake $@
     ;;
 all)
-    C c autotools -C example/libffi-git $@
-    C c cmake -C example/zlib-git $@
-    C c cmake -C example/zlib-http $@
-    C c cmake -C example/libexpat-subpath $@
+    C c autotools -C example/libffi-git-at $@
+    C c cmake -C example/zlib-git-cmake $@
+    C c cmake -C example/zlib-http-cmake $@
+    C c cmake -C example/libexpat-subpath-cmake $@
     ;;
 *)
     echo "Usage: $0 {autotools|cmake|http|subpath|all}"
