acf1e8427ba37a6b2b79557ff9f23043ee129d73
CaiJingLong <cjl_spy@163.com>
2023-04-12 11:49:25 +0800

    ✅ Add test

--- test.sh
+++ test.sh
@@ -11,27 +11,27 @@ shift 1
 
 case $TYPE in
 autotools | at)
-    ## Compile autotools example (libffi)
-    C c autotools -C example/libffi-git-at $@
+    C c autotools -C example/git-autotools/libffi $@
     ;;
 cmake | cm)
     C c cmake -C example/git-cmake/zlib $@
     ;;
 http)
-    C c cmake -C example/zlib-http-cmake $@
+    C c cmake -C example/http-cmake/zlib $@
     ;;
 subpath)
     C c cmake -C example/git-cmake/libexpat-subpath $@
     ;;
 meson)
     # C c meson -C example/git-meson/glib $@
-    C c meson -C example/git-meson/mesa $@
+    # C c meson -C example/git-meson/mesa $@
     ;;
 all)
-    C c autotools -C example/libffi-git-at $@
-    C c cmake -C example/zlib-git-cmake $@
-    C c cmake -C example/zlib-http-cmake $@
-    C c cmake -C example/libexpat-git-subpath-cmake $@
+    $0 autotools $@
+    $0 cmake $@
+    $0 http $@
+    $0 subpath $@
+    $0 meson $@
     ;;
 *)
     echo "Usage: $0 {autotools|cmake|http|subpath|all}"
